e0853975158f6973a39e0c88cd096b16de44a45b
Tim <tspalmieri2018@gmail.com>
2020-01-22 17:17:49 -0500

    Classwork

--- app/my_game.py
+++ app/my_game.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
     print("THANKS, PLEASE PLAY AGAIN!")
     print("-------------------")
 
-    #breakpoint()
-    #print(dir(random))
-    #print(help(random.choice)) # then press "q" to quit
-    #print(type(options))
-    #print(type(user_choice))
+    breakpoint()
+    print(dir(random))
+    print(help(random.choice)) # then press "q" to quit
+    print(type(options))
+    print(type(user_choice))
---
+++ app/my_script.py
@@ -0,0 +1,24 @@
+#some english message
+
+print("Hello World", "Hello")
+
+x = 5
+print(x)
+x = x+100
+print(x)
+
+message = "my message"
+print("Hello World " + "Some other message " + message)
+
+print("Hello World" + f" Some other message {message}")
+
+day = "Monday"
+print(day)
+day = "Tuesday"
+print(day)
+day = day + " Weekend yet?"
+print(day)
+
+
+print(2==4)
+
